c1446ffd62bad2f0a5e8dc257ed9c7fd80da3bb8
akshay123-pixle <ajadhao9696@gmail.com>
2025-04-04 20:14:31 +0530

    app

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {connectDB} from "./databse/mongodb.js";
 import dotenv from "dotenv";
-import { app } from "./app.js";
+import  app  from "./app.js";
 
 dotenv.config();
 connectDB();
